057f3bee90be4c913a51a66ec56df13320f5f5f3
Peter Ahumuza <peter@debian.debian>
2022-09-17 13:34:23 +0300

    Changes to be committed: 	modified:   index.php

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
 <?php 
-echo parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ;
+$path =  parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ;
+
+$parts = explode('/',$path);
+
+$resource = $parts[2];
+$id = $parts[3] ?? null;
+
+if($resource != 'tasks'){
+    http_response_code(404);
+    return;
+}
+echo $_SERVER['REQUEST_METHOD'];
